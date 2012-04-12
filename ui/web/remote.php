445fe27f890de8f568242c9f126f8c6457466a94
Kursad Oney <kursad.oney@freescale.com>
2012-04-12 15:12:59 -0400

    web: Code refactoring + add command status as colors.
    This change creates a new function to send a command and check
    the return code from the TV. The return code is reflected to
    the user using the style element of the button. When the TV
    returns OK, the button is colored GREEN. When ERR, it's RED.

--- ui/web/remote.php
+++ ui/web/remote.php
@@ -8,33 +8,46 @@
 </head>
 <body>
 <?php
+function send_command($button, $cmd, $fh)
+{
+	if (!empty($_POST[$button])) {
+		fwrite($fh, $cmd);
+		$ret = fgets($fh);
+
+		if ($ret === "OK\n") {
+			return "height:80px; width:150px; background-color:#008000";
+		} else if ($ret === "ERR\n") {
+			return "height:80px; width:150px; background-color:#ff0000";
+		}
+		return "height:80px; width:150px; background-color:#008ccc";
+	}
+	return "height:80px; width:150px; background-color:#add8e6";
+}
+
+$style = array(
+	"height:80px; width:150px; background-color:#add8e6",
+	"height:80px; width:150px; background-color:#add8e6",
+	"height:80px; width:150px; background-color:#add8e6",
+	"height:80px; width:150px; background-color:#add8e6",
+);
+
 $out = "/dev/ttyUSB0";
-$fh = fopen($out, 'w') or die("File open error");
+$fh = fopen($out, 'r+') or die("File open error");
 
-if (!empty($_POST['power_on'])) {
-	fwrite($fh, "POWR1   \n");
-}
+$style[0] = send_command('power_on', "POWR1   \n", $fh);
+send_command('power_off', "RSPW1   \n", $fh); /* make sure we can wake it back up */
+usleep(500000);
+$style[1] = send_command('power_off', "POWR0   \n", $fh);
+$style[2] = send_command('chup', "CHUP1   \n", $fh);
+$style[3] = send_command('chdw', "CHDW1   \n", $fh);
 
-if (!empty($_POST['power_off'])) {
-	fwrite($fh, "RSPW1   \n"); /* make sure we can wake it back up */
-	usleep(500000);
-	fwrite($fh, "POWR0   \n");
-}
-
-if (!empty($_POST['chup'])) {
-	fwrite($fh, "CHUP1   \n");
-}
-
-if (!empty($_POST['chdw'])) {
-	fwrite($fh, "CHDW1   \n");
-}
 fclose($fh);
 ?>
 <form method="post">
-	<input type="submit" style="height:80px; width:150px" name="power_on" value="Power On" />
-	<input type="submit" style="height:80px; width:150px" name="power_off" value="Power Off" />
+	<input type="submit" style="<?php echo $style[0]; ?>" name="power_on" value="Power On" />
+	<input type="submit" style="<?php echo $style[1]; ?>" name="power_off" value="Power Off" />
 	<br><br>
-	<input type="submit" style="height:80px; width:150px" name="chup" value="Channel Up" />
-	<input type="submit" style="height:80px; width:150px" name="chdw" value="Channel Down" />
+	<input type="submit" style="<?php echo $style[2]; ?>" name="chup" value="Channel Up" />
+	<input type="submit" style="<?php echo $style[3]; ?>" name="chdw" value="Channel Down" />
 </form>
 </body></html>
