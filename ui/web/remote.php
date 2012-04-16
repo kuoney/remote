5a31d6afacb43ca7559f2f1f1f9a4dea815d780b
Kursad Oney <kursad.oney@freescale.com>
2012-04-16 16:31:00 -0400

    web: Add options to change input.
    The input can now be changed via web. It should actually
    work with siri too. The input settings are:
    	input1 -> iphone dock
    	input9 -> dvd
    	tv     -> tv
    
    I also had to adjust the button width because there are only
    three input modes. It now becomes a part of the send_command
    function. The function now looks too hacked...

--- ui/web/remote.php
+++ ui/web/remote.php
@@ -8,35 +8,39 @@
 </head>
 <body>
 <?php
-function send_command($button, $cmd, $fh)
+function send_command($button, $cmd, $fh, $width)
 {
 	if (!empty($_POST[$button])) {
 		fwrite($fh, $cmd);
 		$ret = fgets($fh);
 
 		if ($ret === "OK\n") {
-			return "height:80px; width:150px; background-color:#008000";
+			return "height:80px; width:${width}px; background-color:#008000";
 		} else if ($ret === "ERR\n") {
-			return "height:80px; width:150px; background-color:#ff0000";
+			return "height:80px; width:${width}px; background-color:#ff0000";
 		}
-		return "height:80px; width:150px; background-color:#008ccc";
+		return "height:80px; width:${width}px; background-color:#008ccc";
 	}
-	return "height:80px; width:150px; background-color:#add8e6";
+	return "height:80px; width:${width}px; background-color:#add8e6";
 }
 
 $style = array_fill(0, 8, "height:80px; width:150px; background-color:#add8e6");
+$style = array_fill(8, 3, "height:80px; width:90px; background-color:#add8e6");
 
 $out = "/dev/ttyUSB0";
 $fh = fopen($out, 'r+') or die("File open error");
 
-$style[0] = send_command('power_on', "POWR1   \n", $fh);
-$style[1] = send_command('power_off', "POWR0   \n", $fh);
-$style[2] = send_command('chup', "CHUP1   \n", $fh);
-$style[3] = send_command('chdw', "CHDW1   \n", $fh);
-$style[4] = send_command('nbc', "DA2P1701\n", $fh);
-$style[5] = send_command('abc', "DA2P1101\n", $fh);
-$style[6] = send_command('fox', "DA2P5001\n", $fh);
-$style[7] = send_command('cbs', "DA2P0501\n", $fh);
+$style[0] = send_command('power_on', "POWR1   \n", $fh, 150);
+$style[1] = send_command('power_off', "POWR0   \n", $fh, 150);
+$style[2] = send_command('chup', "CHUP1   \n", $fh, 150);
+$style[3] = send_command('chdw', "CHDW1   \n", $fh, 150);
+$style[4] = send_command('nbc', "DA2P1701\n", $fh, 150);
+$style[5] = send_command('abc', "DA2P1101\n", $fh, 150);
+$style[6] = send_command('fox', "DA2P5001\n", $fh, 150);
+$style[7] = send_command('cbs', "DA2P0501\n", $fh, 150);
+$style[8] = send_command('tv', "ITVD0   \n", $fh, 90);
+$style[9] = send_command('iphone', "IAVD1   \n", $fh, 90);
+$style[10] = send_command('dvd', "IAVD9   \n", $fh, 90);
 
 fclose($fh);
 ?>
@@ -53,5 +57,10 @@ fclose($fh);
 	<br /><br />
 	<input type="submit" style="<?php echo $style[6]; ?>" name="fox" value="FOX" />
 	<input type="submit" style="<?php echo $style[7]; ?>" name="cbs" value="CBS" />
+	<br /><hr />
+	<p font=verdana> Input Selection </p>
+	<input type="submit" style="<?php echo $style[8]; ?>" name="tv" value="TV" />
+	<input type="submit" style="<?php echo $style[9]; ?>" name="iphone" value="IPHONE" />
+	<input type="submit" style="<?php echo $style[10]; ?>" name="dvd" value="DVD" />
 </form>
 </body></html>
