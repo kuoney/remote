584dc1d0e5801ed8aee8d013fe039fc60a430c90
Kursad Oney <kuoney@gmail.com>
2014-01-10 15:47:15 -0500

    remote: New and shiny buttons.
    This change introduces the new and shiny buttons to make the UI
    prettier. It uses css and overlaying to make the text over
    buttons work.

--- ui/web/remote.php
+++ ui/web/remote.php
@@ -4,63 +4,252 @@
 
 <html>
 <head>
-	<title>Web Remote</title>
+        <title>Web Remote</title>
 </head>
-<body>
-<?php
-function send_command($button, $cmd, $fh, $width)
-{
-	if (!empty($_POST[$button])) {
-		fwrite($fh, $cmd);
-		$ret = fgets($fh);
-
-		if ($ret === "OK\n") {
-			return "height:80px; width:${width}px; background-color:#008000";
-		} else if ($ret === "ERR\n") {
-			return "height:80px; width:${width}px; background-color:#ff0000";
-		}
-		return "height:80px; width:${width}px; background-color:#008ccc";
-	}
-	return "height:80px; width:${width}px; background-color:#add8e6";
+<style>
+.outer {
+    width: 310px;
+    margin: auto;
+    float: left;
+    clear: left;
 }
+.container {
+    position:relative;
+    height:85px;
+    width:155px;
+    margin:0 auto;
+    float:left;
+}
+.container2 {
+    position:relative;
+    height:85px;
+    width:77px;
+    margin:0 auto;
+    float:left;
+}
+.caption {
+    position:absolute;
+    padding:1px;
+    top:35%;
+    width: 200%;
+    left: -50%;
+    color: black;
+    font: 14px Helvetica, Sans-Serif;
+    text-align:center;
+}
+.wraptocenter .wrimg {
+    display: table-cell;
+    text-align: center;
+    vertical-align: middle;
+    width: 155px;
+    height: 85px;
+}
+.wraptocenter2 .wrimg2 {
+    display: table-cell;
+    text-align: center;
+    vertical-align: middle;
+    width: 77px;
+    height: 85px;
+}
+.wraptocenter .wrapcenter2 * {
+    vertical-align: middle;
+}
+.wraptocenter .wrapcenter2 {
+    display: block;
+}
+.wraptocenter .wrapcenter2 span {
+    display: inline-block;
+    height: 100%;
+    width: 1px;
+}
+.wraptocenter .wrimg img  {
+    max-width:155px;
+    max-height:85px;
+}
+.wraptocenter2 .wrimg2 img  {
+    max-width:77px;
+    max-height:85px;
+}
+.shrinkwrapImage {
+    border-radius:0.5em;
+    position : relative;
+    display: inline-block;
+    overflow: hidden;
+}
+</style>
+</body>
+<?php
+function send_command($button, $cmd, $fh)
+{
+        if (!empty($_POST[$button])) {
+                fwrite($fh, $cmd);
+                $ret = fgets($fh);
 
-$style = array_fill(0, 8, "height:80px; width:150px; background-color:#add8e6");
-$style = array_fill(8, 3, "height:80px; width:90px; background-color:#add8e6");
-
+                if ($ret === "OK\n") {
+                        return "./buttons/green.png";
+                } else if ($ret === "ERR\n") {
+                        return "./buttons/red.png";
+                }
+                return "./buttons/yellow.png";
+        }
+        return "./buttons/yellow.png";
+}
 $out = "/dev/ttyUSB0";
 $fh = fopen($out, 'r+') or die("File open error");
 
-$style[0] = send_command('power_on', "POWR1   \n", $fh, 150);
-$style[1] = send_command('power_off', "POWR0   \n", $fh, 150);
-$style[2] = send_command('chup', "CHUP1   \n", $fh, 150);
-$style[3] = send_command('chdw', "CHDW1   \n", $fh, 150);
-$style[4] = send_command('nbc', "DA2P1701\n", $fh, 150);
-$style[5] = send_command('abc', "DA2P1101\n", $fh, 150);
-$style[6] = send_command('fox', "DA2P5001\n", $fh, 150);
-$style[7] = send_command('cbs', "DA2P0501\n", $fh, 150);
-$style[8] = send_command('tv', "ITVD0   \n", $fh, 90);
-$style[9] = send_command('iphone', "IAVD1   \n", $fh, 90);
-$style[10] = send_command('dvd', "IAVD7   \n", $fh, 90);
+$button = array_fill(0, 12, "./buttons/yellow.png");
+$button[0] = send_command('power_on', "POWR1   \n", $fh);
+$button[1] = send_command('power_off', "POWR0   \n", $fh);
+$button[2] = send_command('chup', "CHUP1   \n", $fh);
+$button[3] = send_command('chdw', "CHDW1   \n", $fh);
+$button[4] = send_command('nbc', "DA2P1701\n", $fh);
+$button[5] = send_command('abc', "DA2P1101\n", $fh);
+$button[6] = send_command('fox', "DA2P5001\n", $fh);
+$button[7] = send_command('cbs', "DA2P0501\n", $fh);
+$button[8] = send_command('tv', "ITVD0   \n", $fh);
+$button[9] = send_command('iphone', "IAVD1   \n", $fh);
+$button[10] = send_command('dvd', "IAVD7   \n", $fh);
+$button[11] = send_command('chrome', "IAVD8   \n", $fh);
 
 fclose($fh);
 ?>
 <form method="post">
-	<input type="submit" style="<?php echo $style[0]; ?>" name="power_on" value="Power On" />
-	<input type="submit" style="<?php echo $style[1]; ?>" name="power_off" value="Power Off" />
-	<br /><br />
-	<input type="submit" style="<?php echo $style[2]; ?>" name="chup" value="Channel Up" />
-	<input type="submit" style="<?php echo $style[3]; ?>" name="chdw" value="Channel Down" />
-	<br /><hr />
-	<p font=verdana> Popular Channels </p>
-	<input type="submit" style="<?php echo $style[4]; ?>" name="nbc" value="NBC" />
-	<input type="submit" style="<?php echo $style[5]; ?>" name="abc" value="ABC" />
-	<br /><br />
-	<input type="submit" style="<?php echo $style[6]; ?>" name="fox" value="FOX" />
-	<input type="submit" style="<?php echo $style[7]; ?>" name="cbs" value="CBS" />
-	<br /><hr />
-	<p font=verdana> Input Selection </p>
-	<input type="submit" style="<?php echo $style[8]; ?>" name="tv" value="TV" />
-	<input type="submit" style="<?php echo $style[9]; ?>" name="iphone" value="IPHONE" />
-	<input type="submit" style="<?php echo $style[10]; ?>" name="dvd" value="DVD" />
-</form>
-</body></html>
+<div class="outer">
+    <div class="container">
+        <div class="wraptocenter"> <span class="wrimg">
+                <div class="shrinkwrapImage">
+                    <div id="caption1" class="caption">Power On</div>
+                    <input type="image" name= 'power_on' value='power_on' src="<?php echo $button[0]; ?>" style="height:80px; width:150px" />
+                </div>
+            </span>
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="wraptocenter"> <span class="wrimg">
+                <div class="shrinkwrapImage">
+                    <div id="caption2" class="caption">Power Off</div>
+                    <input type="image" name='power_off' value='power_off' src="<?php echo $button[1]; ?>" style="height:80px; width:150px" />
+                </div>
+            </span>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="outer">
+    <div class="container">
+        <div class="wraptocenter"> <span class="wrimg">
+                <div class="shrinkwrapImage">
+                    <div id="caption1" class="caption">Channel Up</div>
+                    <input type="image" name='chup' value='chup' src="<?php echo $button[2]; ?>" style="height:80px; width:150px" />
+                </div>
+            </span>
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="wraptocenter"> <span class="wrimg">
+                <div class="shrinkwrapImage">
+                    <div id="caption2" class="caption">Channel Down</div>
+                    <input type="image" name='chdw' value='chdw' src="<?php echo $button[3]; ?>" style="height:80px; width:150px" />
+                </div>
+            </span>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="outer">
+    <div class="container2">
+        <div class="wraptocenter2"> <span class="wrimg2">
+                <div class="shrinkwrapImage">
+                    <div id="caption1" class="caption">TV</div>
+                    <input type="image" name='tv' value='tv' src="<?php echo $button[8]; ?>" style="height:80px; width:75px" />
+                </div>
+            </span>
+
+        </div>
+    </div>
+    <div class="container2">
+        <div class="wraptocenter2"> <span class="wrimg2">
+                <div class="shrinkwrapImage">
+                    <div id="caption1" class="caption">iPhone</div>
+                    <input type="image" name='iphone' value='iphone' src="<?php echo $button[9]; ?>" style="height:80px; width:75px" />
+                </div>
+            </span>
+
+        </div>
+    </div>
+    <div class="container2">
+        <div class="wraptocenter2"> <span class="wrimg2">
+                <div class="shrinkwrapImage">
+                    <div id="caption1" class="caption">DVD</div>
+                    <input type="image" name='dvd' value='dvd' src="<?php echo $button[10]; ?>" style="height:80px; width:75px" />
+                </div>
+            </span>
+
+        </div>
+    </div>
+    <div class="container2">
+        <div class="wraptocenter2"> <span class="wrimg2">
+                <div class="shrinkwrapImage">
+                    <div id="caption1" class="caption">Chrome</div>
+                    <input type="image" name='chrome' value='chrome' src="<?php echo $button[11]; ?>" style="height:80px; width:75px" />
+                </div>
+            </span>
+
+        </div>
+    </div>
+</div>
+
+
+
+<div class="outer">
+    <div class="container">
+        <div class="wraptocenter"> <span class="wrimg">
+                <div class="shrinkwrapImage">
+                    <div id="caption1" class="caption">NBC</div>
+                    <input type="image" name='nbc' value='nbc' src="<?php echo $button[4]; ?>" style="height:80px; width:150px" />
+                </div>
+            </span>
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="wraptocenter"> <span class="wrimg">
+                <div class="shrinkwrapImage">
+                    <div id="caption2" class="caption">ABC</div>
+                    <input type="image" name='abc' value='abc' src="<?php echo $button[5]; ?>" style="height:80px; width:150px" />
+                </div>
+            </span>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="outer">
+    <div class="container">
+        <div class="wraptocenter"> <span class="wrimg">
+                <div class="shrinkwrapImage">
+                    <div id="caption1" class="caption">Fox</div>
+                    <input type="image" name='fox' value='fox' src="<?php echo $button[6]; ?>" style="height:80px; width:150px" />
+                </div>
+            </span>
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="wraptocenter"> <span class="wrimg">
+                <div class="shrinkwrapImage">
+                    <div id="caption2" class="caption">CBS</div>
+                    <input type="image" name='cbs' value='cbs' src="<?php echo $button[7]; ?>" style="height:80px; width:150px" />
+                </div>
+            </span>
+        </div>
+    </div>
+</div>
+</form></body></html>
